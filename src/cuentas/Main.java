36fc56f6528c2d1d79c8d25d537e60e155524b35
Berna <Berna@DESKTOP-2BK0KRE>
2025-02-17 23:15:02 +0000

    Primero

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -22,6 +22,8 @@ System.out.println("El saldo actual es " + saldoActual);
         operaciones(cuenta1);
 saldoActual = cuenta1.estado();
 System.out.println("El saldo actual es " + saldoActual);
+
+System.out.println("Saldo actualizado: " + cuenta1.estado());
 }
 
     private static void operaciones(CCuenta cuenta1) {
